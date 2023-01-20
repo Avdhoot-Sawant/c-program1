68af35f115e13d164c95bfc386dfdc4ff334b90c
Avdhoot-Sawant <109811842+Avdhoot-Sawant@users.noreply.github.com>
2023-01-20 21:13:23 +0530

    Create sumNumber.c

---
+++ sumNumber.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    int  j, sum = 0;
+  
+    for (j = 1; j <= 10; j++)
+    {
+        sum = sum + j;       
+    }
+    printf("\nThe Sum is : %d\n", sum);
+}
+
+
+
